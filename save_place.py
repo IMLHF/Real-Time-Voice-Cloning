58f68795f9b39552dc88a469afa303f786693635
IMLHF <red_wind@foxmail.com>
2019-08-16 10:47:56 +0800

    add take place code

---
+++ save_place.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+config = tf.ConfigProto()
+config.allow_soft_placement = True
+config.gpu_options.allow_growth = False
+config.gpu_options.per_process_gpu_memory_fraction = 0.3
+# w = tf.get_variable("w",[10000,100000])
+init = tf.global_variables_initializer()
+sess = tf.Session(config=config)
+sess.run(init)
+# w = sess.run(w)
+# print(w)
