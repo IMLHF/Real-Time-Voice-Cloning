afa1c0f35c7fc88d89a238c16506db40a32291a2
IMLHF <red_wind@foxmail.com>
2019-08-16 14:10:38 +0800

    update

--- save_place.py
+++ save_place.py
@@ -1,12 +1,14 @@
 import tensorflow as tf
 
 config = tf.ConfigProto()
-config.allow_soft_placement = True
-config.gpu_options.allow_growth = False
-config.gpu_options.per_process_gpu_memory_fraction = 0.3
+# config.allow_soft_placement = True
+# config.gpu_options.allow_growth = False
+config.gpu_options.per_process_gpu_memory_fraction = 0.43
 # w = tf.get_variable("w",[10000,100000])
 init = tf.global_variables_initializer()
 sess = tf.Session(config=config)
 sess.run(init)
+while True:
+    input()
 # w = sess.run(w)
 # print(w)
