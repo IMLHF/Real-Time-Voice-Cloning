7405b0648d46a1741ca77c7ee810dfe8b75196b9
IMLHF <red_wind@foxmail.com>
2019-08-22 10:10:54 +0800

    add symbols

--- synthesizer/utils/symbols.py
+++ synthesizer/utils/symbols.py
@@ -6,9 +6,9 @@ through Unidecode. For other data, you can modify _characters. See TRAINING_DATA
 """
 # from . import cmudict
 
-_pad        = "_"
-_eos        = "~"
-_characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!\"\"(),-.:;? "
+_pad = "_"
+_eos = "~"
+_characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!/\"\"(),-.:;?# "
 
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
 #_arpabet = ["@' + s for s in cmudict.valid_symbols]
