fba8b345904729603f48868ea9af4147bebee2bb
IMLHF <red_wind@foxmail.com>
2019-09-10 20:05:14 +0800

    20G GPU get embedding

--- synthesizer_preprocess_embeds.py
+++ synthesizer_preprocess_embeds.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     parser.add_argument("-e", "--encoder_model_fpath", type=Path,
                         default="encoder/saved_models/pretrained.pt",
                         help="Path your trained encoder model.")
-    parser.add_argument("-n", "--n_processes", type=int, default=4,
+    parser.add_argument("-n", "--n_processes", type=int, default=26,
                         help="Number of parallel processes. An encoder is created for each, so you may need to lower "
                         "this value on GPUs with low memory. Set it to 1 if CUDA is unhappy.")
     args = parser.parse_args()
